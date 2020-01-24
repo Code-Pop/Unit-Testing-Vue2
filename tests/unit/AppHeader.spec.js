9010341615d09aa42db5a1aaaf8cf7e48798dd41
Adam Jahr <adamjahr@gmail.com>
2020-01-24 12:52:39 -0500

    Lesson 2 - start

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,15 +1,1 @@
-import AppHeader from '@/components/AppHeader'
-import { mount } from '@vue/test-utils'
-
-describe('AppHeader', () => {
-  test('If user is not logged in, do not show logout button', () => {
-    const wrapper = mount(AppHeader)
-    expect(wrapper.find('button').isVisible()).toBe(false)
-  })
-
-  test('If user is logged in, show logout button', () => {
-    const wrapper = mount(AppHeader)
-    wrapper.setData({ loggedIn: true })
-    expect(wrapper.find('button').isVisible()).toBe(true)
-  })
-})
+// write your tests here
