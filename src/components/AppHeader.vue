f95209b1b469cae9a770131bbd3f94a139800dea
Adam Jahr <adamjahr@gmail.com>
2020-01-24 12:41:23 -0500

    Lesson 2 - ending code

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false
+    }
+  }
+}
+</script>
