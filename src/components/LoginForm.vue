f2929141354853da0652fbae9b95a5db0eb4eb70
Adam Jahr <adamjahr@gmail.com>
2020-03-26 00:06:47 -0400

    lesson 4 start update

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="name" />
+    <input data-testid="name-input" type="text" v-model="name" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('formSubmitted', { name: this.name })
+    }
+  }
+}
+</script>
