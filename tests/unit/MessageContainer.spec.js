3e0a05845e8b92fc7deed4fe62495cf3837fa8a2
Adam Jahr <adamjahr@gmail.com>
2020-05-05 02:37:38 -0400

    lesson 6 starting

--- tests/unit/MessageContainer.spec.js
+++ tests/unit/MessageContainer.spec.js
@@ -3,13 +3,6 @@ import { mount } from '@vue/test-utils'
 
 describe('MessageContainer', () => {
   it('Wraps MessageDisplay component', () => {
-    const wrapper = mount(MessageContainer, {
-      stubs: {
-        MessageDisplay: '<p data-testid="message">Hello from the db!</p>'
-      }
-    })
-
-    const message = wrapper.find('[data-testid="message"]').element.textContent
-    expect(message).toEqual('Hello from the db!')
+    const wrapper = mount(MessageContainer)
   })
 })
